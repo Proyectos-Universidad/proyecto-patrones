b1f802ac4267630b0ada85720a38e70e02f363b7
lotusmz <mary_zap@hotmail.com>
2018-07-08 10:24:37 -0600

    Creación ModelsTestSuite y Override del equals

---
+++ proyecto-programacion-con-patrones-grupal/Workflow_Engine_Model/src/ac/cr/ucenfotec/workflowengine/modelstest/ModelsTestSuite.java
@@ -0,0 +1,13 @@
+package ac.cr.ucenfotec.workflowengine.modelstest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+
+})
+
+public class ModelsTestSuite {
+
+}
