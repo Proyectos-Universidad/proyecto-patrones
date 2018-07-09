22fe30d4855df618b57c7a9f296e39ad51c47fe9
Mari Paz Araya <maripaz.a@local1.gca>
2018-07-08 20:00:50 -0600

    AssigneeRecord Override Equals

--- proyecto-programacion-con-patrones-grupal/Workflow_Engine_Model/src/ac/cr/ucenfotec/workflowengine/models/workflow/AssigneeRecord.java
+++ proyecto-programacion-con-patrones-grupal/Workflow_Engine_Model/src/ac/cr/ucenfotec/workflowengine/models/workflow/AssigneeRecord.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import ac.cr.ucenfotec.workflowengine.models.form.Form;
 @Entity
 public class AssigneeRecord {
 	@Id
@@ -56,5 +58,17 @@ public class AssigneeRecord {
 		this.guide = index;
 	}
 	
-	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null)
+			return false;
+		if(!(o instanceof AssigneeRecord))
+			return false;
+		
+		AssigneeRecord assigneeRecord = (AssigneeRecord) o;
+		
+		return getId() == assigneeRecord.getId(); 
+	}
 }
