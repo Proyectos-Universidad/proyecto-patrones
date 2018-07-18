ba0b4b517295cc11a5db0c25eecfefcfe00b21a7
Julio <jforerom@ucenfotec.ac.cr>
2018-07-17 19:10:44 -0600

    Added WorkflowServiceTest to SuiteClasses

--- Workflow_Engine_BL/src/ac/cr/ucenfotec/workflowengine/businesslogictest/BusinessLogicTestSuite.java
+++ Workflow_Engine_BL/src/ac/cr/ucenfotec/workflowengine/businesslogictest/BusinessLogicTestSuite.java
@@ -9,7 +9,8 @@ import ac.cr.ucenfotec.workflowengine.dao.HUtil;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	FunctionalAreaServiceTest.class,
-	UserServiceTest.class
+	UserServiceTest.class,
+	WorkflowServiceTest.class
 })
 
 public class BusinessLogicTestSuite {
