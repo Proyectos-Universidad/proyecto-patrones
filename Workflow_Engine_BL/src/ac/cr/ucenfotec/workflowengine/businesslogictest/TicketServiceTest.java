a8126cc11af490d877f9ed4810bd26c01948e322
Julio <jforerom@ucenfotec.ac.cr>
2018-07-18 22:54:26 -0600

    Added ticket service unit tests

--- Workflow_Engine_BL/src/ac/cr/ucenfotec/workflowengine/businesslogictest/TicketServiceTest.java
+++ Workflow_Engine_BL/src/ac/cr/ucenfotec/workflowengine/businesslogictest/TicketServiceTest.java
@@ -1,13 +1,19 @@
 package ac.cr.ucenfotec.workflowengine.businesslogictest;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import ac.cr.ucenfotec.workflowengine.businesslogic.FunctionalAreaService;
 import ac.cr.ucenfotec.workflowengine.businesslogic.TicketService;
 import ac.cr.ucenfotec.workflowengine.businesslogic.UserService;
 import ac.cr.ucenfotec.workflowengine.businesslogic.WorkflowService;
+import ac.cr.ucenfotec.workflowengine.businesslogic.WorkflowStateService;
 import ac.cr.ucenfotec.workflowengine.models.workflow.Ticket;
+import ac.cr.ucenfotec.workflowengine.models.workflow.WorkflowState;
+import ac.cr.ucenfotec.workflowengine.models.workflow.WorkflowStateRecord;
 import ac.cr.ucenfotec.workflowengine.validation.error.WFErrors;
 
 public class TicketServiceTest {
@@ -41,8 +47,12 @@ public class TicketServiceTest {
 	@Test
 	public void nextStateTest() {
 		TicketService ticketService = new TicketService();
-		Ticket ticket = ticketService.getAll().get(0);
-		
+		Ticket ticket = ticketService.get(ticketService.getAll().get(0));
+		int wsrSize = ticket.getProgress().size();
+		ticketService.nextState(errors, ticket);
+		Assert.assertFalse(errors.hasErrors());
+		ticket = ticketService.get(ticket);
+		Assert.assertNotEquals(wsrSize,ticket.getProgress().size());
 		
 	}
 
